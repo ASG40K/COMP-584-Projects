230c351954b076ef54e5c8ba29b0bc5483ec51de
ASG40K <anthonygalindo185@gmail.com>
2025-03-14 18:13:43 -0700

    all current updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { WeatherComponent } from './weather/weather.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CountryPopulationComponent } from './country-population/country-population.component';
 
 export const routes: Routes = [
     { 
@@ -18,8 +19,13 @@ export const routes: Routes = [
     {
         path: "navbar", component: NavBarComponent
     },
+
+    {
+        path: "CountryPopulation/:id", component: CountryPopulationComponent
+    },
     {
         path: "", component: WeatherComponent, pathMatch:"full"
     }
 
+    
 ];
