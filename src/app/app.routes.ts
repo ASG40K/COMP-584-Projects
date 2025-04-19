e29d6214a855e36980bc5ff78172d329e371fc61
ASG40K <anthonygalindo185@gmail.com>
2025-04-18 18:15:35 -0700

    Client changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CountryPopulationComponent } from './country-population/country-population.component';
+import { LoginComponent } from './auth/login.component';
 
 export const routes: Routes = [
     { 
@@ -23,6 +24,12 @@ export const routes: Routes = [
     {
         path: "CountryPopulation/:id", component: CountryPopulationComponent
     },
+
+    {
+        path: "login", component: LoginComponent
+    },
+
+
     {
         path: "", component: WeatherComponent, pathMatch:"full"
     }
